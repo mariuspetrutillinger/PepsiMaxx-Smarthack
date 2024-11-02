dbed7b33c2a9a6b03e40d405091db0f2753cf919
Brioflator <balescu.alexandru2002@gmail.com>
2024-11-02 15:56:59 +0200

    Spring config

---
+++ Smarthack/src/main/java/com/PepsiMaxx/Smarthack/SmarthackApplication.java
@@ -0,0 +1,13 @@
+package com.PepsiMaxx.Smarthack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthackApplication.class, args);
+	}
+
+}
---
+++ Smarthack/src/test/java/com/PepsiMaxx/Smarthack/SmarthackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PepsiMaxx.Smarthack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
